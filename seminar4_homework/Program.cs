ff680115851fc71b75e58a1f9454ecf2fc4a4293
vladimir_rozhkov <r406@ya.ru>
2022-09-27 23:44:12 +0300

    Добавлена доп. задача с семинара 4 - про отрезки

--- seminar4_homework/Program.cs
+++ seminar4_homework/Program.cs
@@ -45,3 +45,75 @@ Console.WriteLine($"{A} ^ {B} = {result}");
 // array[i] = new Random().Next(0, 100); // Последнее число генерируется вне цикла, чтобы включить его в строку без ", "
 // stringArray = stringArray + array[i] + "]";
 // Console.WriteLine(stringArray);
+
+
+// //  Доп. задача семинара: У нас есть график, где даны отрезок 1 -  (х1; x2) и отрезок 2 (х3, x4)
+// // 	​Необходимо составить код, который находит их пересечение.
+// // 	Пересечением двух отрезка 1 и 2 могут быть  - отрезок, точка или ничего
+// // 	С клавиатуры вводим х1, x2, х3, x4 и выводим ответ - границы пересечений отрезков, общую точку или «не пересекаются».
+// Console.WriteLine("Отрезки (х1, х2) и (х3, х4)");
+// Console.Write("x1: ");
+// int x1 = Convert.ToInt32(Console.ReadLine());
+// Console.Write("x2: ");
+// int x2 = Convert.ToInt32(Console.ReadLine());
+// Console.Write("x3: ");
+// int x3 = Convert.ToInt32(Console.ReadLine());
+// Console.Write("x4: ");
+// int x4 = Convert.ToInt32(Console.ReadLine());
+// int temp = 0;
+// if (x1 > x2)
+// {
+//     temp = x1;
+//     x1 = x2;
+//     x2 = temp;
+// }
+// if (x3 > x4)
+// {
+//     temp = x3;
+//     x3 = x4;
+//     x4 = temp;
+// }
+// if (x2 == x3)
+// {
+//     Console.WriteLine("Пересекаются в точке " + x2);
+// }
+// else if (x1 == x4)
+// {
+//     Console.WriteLine("Пересекаются в точке " + x1);
+// }
+// else if (x2 > x3 & x1 < x3)
+// {
+//     if (x2 > x4)
+//     {
+//         Console.WriteLine("Пересекаются в границах " + x3 + ":" + x4);
+//     }
+//     else
+//     {
+//         Console.WriteLine("Пересекаются в границах " + x3 + ":" + x2);
+//     }
+// }
+// else if (x4 > x1 & x3 < x1)
+// {
+//     if (x4 > x2)
+//     {
+//         Console.WriteLine("Пересекаются в границах " + x1 + ":" + x2);
+//     }
+//     else
+//     {
+//         Console.WriteLine("Пересекаются в границах " + x1 + ":" + x4);
+//     }
+// }
+// else Console.WriteLine("Не пересекаются");
+// // Визуализация для проверки корректности алгоритма
+// int min = x1;
+// int max = x2;
+// if (x3 < x1) min = x3;
+// if (x4 > x2) max = x4;
+// Console.WriteLine();
+// for (int i = min; i <= max; i++)
+// {
+//     if (i == x1 || i == x2 || i == x3 || i == x4) Console.Write(i + " ");
+//     else Console.Write("." + " ");
+// }
+// Console.WriteLine();
+// Console.WriteLine();
